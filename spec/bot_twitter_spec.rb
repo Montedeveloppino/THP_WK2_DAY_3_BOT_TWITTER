32168ed8d5caa4a81609fe7913c8eca02f388f07
Youssef <annabiyoussef1986@gmail.com>
2021-01-20 18:15:51 +0100

    updates

--- spec/bot_twitter_spec.rb
+++ spec/bot_twitter_spec.rb
@@ -2,6 +2,6 @@ require_relative '../lib/bot_twitter.rb'
 
 describe "the login_twitter method" do
   it "should return client, and client is not nil" do
-    expect(login_twitter).not_to be_nil
+    expect(twitter_login).not_to be_nil
   end
 end
