3b756aded3729bdcf8172cc86300f39319ab22af
Youssef <annabiyoussef1986@gmail.com>
2021-01-20 10:20:40 +0100

    twitter gem

--- lib/bot_twitter.rb
+++ lib/bot_twitter.rb
@@ -1,8 +1,9 @@
+require 'twitter'
 require 'pry'
 require 'dotenv'
 
 Dotenv.load('../.env')
-
+client.update('Mon premier tweet en Ruby !!!!')
 
 puts ENV['TWITTER_API_SECRET']
 puts ENV['BEST_WEBSITE_EVER']
