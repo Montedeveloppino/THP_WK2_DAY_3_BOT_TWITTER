ae337977a7acb45c2caee9a9098e61aa4749c925
Youssef <annabiyoussef1986@gmail.com>
2021-01-20 12:12:09 +0100

    youssef final

--- lib/bot_twitter.rb
+++ lib/bot_twitter.rb
@@ -1,9 +1,13 @@
 require 'twitter'
-require 'pry'
 require 'dotenv'
 
-Dotenv.load('../.env')
-client.update('Mon premier tweet en Ruby !!!!')
-
-puts ENV['TWITTER_API_SECRET']
-puts ENV['BEST_WEBSITE_EVER']
+  Dotenv.load('../.env')
+def login_twitter
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+client.update('Todays gonna be a hard day !!!!')  
+end
