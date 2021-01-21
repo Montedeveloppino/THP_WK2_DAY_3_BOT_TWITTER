1c6e30d5ce9fcb00bee901e12017b78f45583b91
Youssef <annabiyoussef1986@gmail.com>
2021-01-21 08:14:38 +0100

    last commit

--- lib/bot_follow.rb
+++ lib/bot_follow.rb
@@ -12,6 +12,16 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end 
 
-tweets = client.search("#bonjour_monde", lang: "fr").take(5)
-
-client.follow(tweets)
+  array = []
+  n = 19
+  while array.uniq[19] == nil
+    client.search("#bonjour_monde", result_type: "recent").take(n).collect do |tweet|
+      array << tweet.user.screen_name
+  
+    end
+    new_array = array.uniq
+    n = n + 4
+  end
+  new_array.each do |f|
+   client.follow(f)
+  end
